cb2bb1d1f2d1cde7ba053572387999e7282c2ee0
pearkrub <praibool.piak@gmail.com>
2014-05-31 11:16:28 +0700

    edit1

--- demos/angular-implement/helloworld/js/controller.js
+++ demos/angular-implement/helloworld/js/controller.js
@@ -1,14 +1,15 @@
 function HomeCtrl ($scope) {
 	// body...
 	$scope.message = "Hi scope";
-	$scope.tax = 7
-	$scope.salary = 15000
-	$scope.car = {color:"red", brand:"toyota"}
+	$scope.tax = 7;
+	$scope.salary = 15000;
+	$scope.car = {color:"red", brand:"toyota"};
 }
 
 function ProfileCtrl ($scope) {
 	// body...
 
-	$scope.salary = 15000
+	$scope.salary = 15000;
+        $scope.user = {name:"Praibool",age:21,food:"burger"};
 
 }
